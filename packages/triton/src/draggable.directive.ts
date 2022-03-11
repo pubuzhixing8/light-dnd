55ba80e8422bb3ebc9d998e491732e4792f8b186
pubuzhixing8 <pubuzhixing@gmail.com>
2022-03-11 10:19:01 +0800

    feat: support snapshot

--- packages/triton/src/draggable.directive.ts
+++ packages/triton/src/draggable.directive.ts
@@ -1,9 +1,12 @@
 import { ScrollDispatcher } from "@angular/cdk/scrolling";
-import { AfterViewInit, Directive, ElementRef, EventEmitter, HostListener, NgZone, OnDestroy, Output } from "@angular/core";
+import { AfterViewInit, Directive, ElementRef, EventEmitter, HostListener, Input, NgZone, Output } from "@angular/core";
 import { fromEvent, interval } from "rxjs";
 import { take } from 'rxjs/operators';
 import { ScrollingDirection } from "./constants";
 import { getAutoScrollDirection, getAutoScrollContainers, handleContainerScroll, handleWindowScroll } from "./scroll";
+
+const TRIGGER_DRAG_THRESHOLD = 5;
+
 
 @Directive({
     selector: '[triDraggable]',
@@ -12,6 +15,12 @@ import { getAutoScrollDirection, getAutoScrollContainers, handleContainerScroll,
     }
 })
 export class TriDraggableDirective implements AfterViewInit {
+    public target?: HTMLElement;
+
+    @Input() triGetTargetFn?: (origin: HTMLElement) => HTMLElement;
+
+    @Input() triHasSnapshot = false;
+
     @HostListener('mousedown', ['$event'])
     mousedownHandle(event: MouseEvent) {
         let dragging = false;
@@ -19,6 +28,7 @@ export class TriDraggableDirective implements AfterViewInit {
         const autoScrollContainers = getAutoScrollContainers(this.scrollDispatcher, this.elementRef.nativeElement);
         let currentAutoScrollContainer: HTMLElement | null = null;
         let autoScrollDirection: ScrollingDirection | null = null;
+        const start = [event.clientX, event.clientY];
         this.ngZone.runOutsideAngular(() => {
             const interval$ = interval(20).subscribe(() => {
                 if (autoScrollDirection && currentAutoScrollContainer) {
@@ -31,10 +41,26 @@ export class TriDraggableDirective implements AfterViewInit {
             });
             const mousemove$ = fromEvent<MouseEvent>(document, `mousemove`).subscribe(e => {
                 if (!dragging) {
+                    const end = [e.clientX, e.clientY];
+                    const maxOffset = Math.max(Math.abs(end[0] - start[0]), Math.abs(end[1] - start[0]));
+                    if (maxOffset < TRIGGER_DRAG_THRESHOLD) {
+                        return;
+                    }
+                }
+                if (!dragging) {
                     this.triDragStart.emit(event);
                     dragging = true;
                     document.body.style.cursor = 'grabbing';
                     document.body.classList.add('dragging');
+                    if (this.triGetTargetFn) {
+                        this.target = this.triGetTargetFn(this.elementRef.nativeElement);
+                    }
+                    if (this.target) {
+                        this.target.classList.add(...['drag-and-drop-target', 'drag-and-drop-target-active']);
+                    }
+                    if (this.triHasSnapshot) {
+                        // 弹出 overlay
+                    }
                 }
                 this.triDragOver.emit(e);
                 currentAutoScrollContainer = null;
@@ -57,6 +83,9 @@ export class TriDraggableDirective implements AfterViewInit {
                     this.triDragEnd.emit(e);
                     document.body.style.cursor = defaultCursor;
                     document.body.classList.remove('dragging');
+                    if (this.target) {
+                        this.target.classList.remove(...['drag-and-drop-target', 'drag-and-drop-target-active']);
+                    }
                 }
             });
             // const keydown$ = fromEvent<KeyboardEvent>(this.elementRef.nativeElement, `keydown`).subscribe(e => {
