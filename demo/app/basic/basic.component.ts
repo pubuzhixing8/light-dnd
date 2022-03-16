88e8a55f54e5a6d3b7eaf1a71119f7c060092dd5
pubuzhixing8 <pubuzhixing@gmail.com>
2022-03-16 21:39:25 +0800

    feat: add snapshot

--- demo/app/basic/basic.component.ts
+++ demo/app/basic/basic.component.ts
@@ -1,15 +1,26 @@
-import { Component } from "@angular/core";
+import { Component, ElementRef, ViewChild } from "@angular/core";
 
 @Component({
     selector: 'app-basic',
-    templateUrl: 'basic.component.html',
-    styleUrls: ['basic.component.scss']
+    templateUrl: 'basic.component.html'
 })
 export class AppBasicComponent {
+    @ViewChild('draggableElement', { static: true })
+    draggableElement?: ElementRef<HTMLElement>;
+
+    offsetX = 0;
+
+    offsetY = 0;
+
     constructor() {}
 
-    triDragStart(event: MouseEvent) {
+    triDragStart(e: MouseEvent) {
         console.log('drag start');
+        if (this.draggableElement) {
+            const targetRect = this.draggableElement.nativeElement.getBoundingClientRect();
+            this.offsetX = targetRect.x - e.clientX;
+            this.offsetY = targetRect.y - e.clientY;
+        }
     }
 
     triDragOver(event: MouseEvent) {
@@ -18,5 +29,11 @@ export class AppBasicComponent {
 
     triDragEnd(event: MouseEvent) {
         console.log('drag end');
+        if (this.draggableElement) {
+            let scrollTop = window.scrollY;
+            this.draggableElement.nativeElement.style.left = `${event.clientX + this.offsetX}px`;
+            this.draggableElement.nativeElement.style.top = `${event.clientY + this.offsetY + scrollTop}px`;
+            this.draggableElement.nativeElement.style.transform = `none`;
+        }
     }
 }
