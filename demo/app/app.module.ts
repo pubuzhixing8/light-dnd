44c06bb6caf41ae3e7810f0c663c1616dda22e30
pubuzhixing8 <pubuzhixing@gmail.com>
2022-03-21 11:35:42 +0800

    feat: add inner scroll demo

--- demo/app/app.module.ts
+++ demo/app/app.module.ts
@@ -1,3 +1,4 @@
+import { ScrollingModule } from '@angular/cdk/scrolling';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { TritonModule } from 'light-dnd';
@@ -5,16 +6,19 @@ import { TritonModule } from 'light-dnd';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AppBasicComponent } from './basic/basic.component';
+import { AppInnerScrollComponent } from './inner-scroll/inner-scroll.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AppBasicComponent
+    AppBasicComponent,
+    AppInnerScrollComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    TritonModule
+    TritonModule,
+    ScrollingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
